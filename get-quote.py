c7e19bba4b110c0a141e7ce75b95d9c45251994c
Renee Selvey <u6660447@anu.edu.au>
2021-08-11 14:22:59 +1000

    Finished all tasks

--- get-quote.py
+++ get-quote.py
@@ -1,12 +1,16 @@
 import random
 def primary():    
-    f = open("quotes.txt")
+    f = open("quotes.txt", 'r+')
+    f.write("I am Renee \n")
     quotes = f.readlines()
     f.close()
     
-    last = 13
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
+    last = len(quotes)-1
+    num_quotes = 2
+    for i in range(num_quotes):
+        rnd = random.randint(0, last)
+        quote = quotes[rnd].rstrip("\n")
+        print(quote)
 
 if __name__== "__main__":
     primary()
